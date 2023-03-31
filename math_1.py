b5c82d2306078fb401711d25b99aabe170ed2f2a
Masian Parkire <parkiremasian@gmail.com>
2023-03-31 16:36:33 +0300

    pythonfunctions

---
+++ math_1.py
@@ -0,0 +1,19 @@
+def addition(num1,num2):
+    answer = num1 +num2
+    return answer
+
+def sub(a,b):
+    answer = a - b
+    return answer
+
+def multiply(c,d):
+    answer = c * d
+    return answer
+
+def division(x,y):
+    answer = x/ y
+    return answer
+
+def remainder(num5,num6):
+    answer = num5% num6
+    return answer
